0f705068697e479daa2a7d9cca290551662bea28
mbriatte <mbriatte@gmail.com>
2016-06-23 14:10:56 +0100

    creation des classes JUNITtestSuite

---
+++ src/test/com/mika/qcm/service/AllTests.java
@@ -0,0 +1,11 @@
+package test.com.mika.qcm.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ QuestionnaireServiceImplTest.class, QuestionServiceImplTest.class })
+public class AllTests {
+
+}
