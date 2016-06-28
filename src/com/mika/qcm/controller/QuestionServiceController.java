5f192742a156b54eca4596ea82dedeced1f89522
mbriatte <mbriatte@gmail.com>
2016-06-28 14:24:53 +0100

    creation de questionservicecontroller

---
+++ src/com/mika/qcm/controller/QuestionServiceController.java
@@ -0,0 +1,34 @@
+package com.mika.qcm.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mika.qcm.model.Question;
+import com.mika.qcm.service.QuestionService;
+
+
+@Controller
+@RestController 
+public class QuestionServiceController {
+	
+	@Autowired 
+	private QuestionService questionService; 
+	
+	@RequestMapping(value = "/questions", method = RequestMethod.GET) 
+	public @ResponseBody  ResponseEntity<Object> getQuestions() {		
+		
+			List<Question> q=questionService.getQuestions();	
+			return new ResponseEntity<Object>(q, HttpStatus.OK);
+		
+		} 
+
+}
