bf39674a0f20d9a68318761564844d0067650680
mbriatte <mbriatte@gmail.com>
2016-07-11 13:59:35 +0100

    ajout des classes techniques pour gerer JWT

--- src/com/mika/jwt/JwtGenerator.java
+++ src/com/mika/jwt/JwtGenerator.java
@@ -5,34 +5,63 @@ import javax.xml.bind.DatatypeConverter;
 
 
 import java.security.Key;
+import java.security.SecureRandom;
+
 import io.jsonwebtoken.*;
 
 import java.util.Base64;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 import java.util.Date;
+
+
+//"https://libraries.io/github/robertjd/jjwt"
 
 public class JwtGenerator {
 	
-	public static String createJWT(String id, String issuer, String subject, long ttlMillis) {
+	private static String NORMALKEY="eyJraWQiOiJub3JtYWwiLCJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxIiwiaWF0IjoxNDY4MjMxOTAyLCJzdWIiOiJhY2NlcyIsImlzcyI6Iklzc3VlcjEiLCJhdWQiOiJRQ00iLCJwYXlsb2FkIjoidGhpcyBpcyBhIHBheWxvYWQiLCJleHAiOjE0NjgyMzI5MDJ9.SgfDSx-aQNr7S_baqKLJJdmPoL9FVOpHtWouVAW3l90";
+	private static String SPECIALKEY="eyJraWQiOiJzcGVjaWFsIiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJqdGkiOiIyIiwiaWF0IjoxNDY4MjMxOTAyLCJzdWIiOiJhY2Nlc3MiLCJpc3MiOiJJc3N1ZXIyIiwiYXVkIjoiUUNNIiwicGF5bG9hZCI6InRoaXMgaXMgYSBwYXlsb2FkIiwiZXhwIjoxNDY4MjMyOTAyfQ.JS3jQnoSa6yNrO5dnSrKbz3JIIrKgQ77xdY-oFyyQc0";
+
+	
+	public static String createJWT(String id, String issuer, String subject, long ttlMillis, Boolean withSpecialKey) {
 		 
 		//The JWT signature algorithm we will be using to sign the token
 		SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 		 
 		long nowMillis = System.currentTimeMillis();
 		Date now = new Date(nowMillis);
-		 
+		byte[] apiKeySecretBytes=null;
+		
 		//We will sign our JWT with our ApiKey secret		
-		byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(generateKey());
+		String type_kid="";
+		if (withSpecialKey) // pour certain on va utiliser une clef special
+		{
+			apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString(JwtGenerator.SPECIALKEY.getBytes()));
+			type_kid="VIP";
+		}
+		else  // tout les autres auront une clef "normal"
+			{
+			apiKeySecretBytes = DatatypeConverter.parseBase64Binary(generateKey());
+			type_kid="NORMAL";
+			}
+		
 		Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 		 
 		  //Let's set the JWT Claims
-		JwtBuilder builder = Jwts.builder().setId(id)
-		                                .setIssuedAt(now)
-		                                .setSubject(subject)
-		                                .setIssuer(issuer)
-		                                .signWith(signatureAlgorithm, signingKey);
+		JwtBuilder builder = Jwts.builder().setId(id) // l'id du jeton -- unique de preference
+		                                .setIssuedAt(now) // est valable à partir de 
+		                                .setSubject(subject)// le sujet poru lequel il est generé
+		                                .setIssuer(issuer) // celui qu'il l'emet
+		                                .setAudience("QCM") // a qui il est destiné
+		                                .setHeaderParam("kid", type_kid) // l'indice poru savoir quelle clef utilisée pour le chiffrement
+		                                .claim("payload","this is a payload") // le payload eventuelle
+		                                .signWith(signatureAlgorithm, signingKey); // la signature
 		 
 
-		builder.setHeaderParam("typ", new String("JWT"));
+		builder.setHeaderParam("typ", new String("JWT")); // le type de jeton
 		 //if it has been specified, let's add the expiration
 		if (ttlMillis >= 0) {
 		    long expMillis = nowMillis + ttlMillis;
@@ -46,11 +75,11 @@ public class JwtGenerator {
 	
 	public static String generateKey()
 	{
-		return Base64.getEncoder().encodeToString("abcde".getBytes());
+		return Base64.getEncoder().encodeToString(JwtGenerator.NORMALKEY.getBytes());
 	}
 	
 	
-	public static String parseJWT(String jwt) {
+	public static Claims  parseJWTwithKey(String jwt) {
 		//This line will throw an exception if it is not a signed JWS (as expected)
 		Claims claims = Jwts.parser()         
 		   .setSigningKey(DatatypeConverter.parseBase64Binary(generateKey()))
@@ -59,7 +88,42 @@ public class JwtGenerator {
 		System.out.println("Subject: " + claims.getSubject());
 		System.out.println("Issuer: " + claims.getIssuer());
 		System.out.println("Expiration: " + claims.getExpiration());
-
-		return "ok";
+		System.out.println("payload: " + claims.get("payload", String.class));
+		return claims;
 	}
+	
+	public static Claims  parseJWTwithKeyhintinKeyID(String jwt) {
+		//This line will throw an exception if it is not a signed JWS (as expected)
+		SigningKeyResolver resolver = new SigningKeyResolverAdapter();
+		 
+		
+		// attention c'ets space !!!,
+		//on va definir inline la classe SigningKeyResolverAdapter
+		//qui va permettre dynamiquement de charger la clef de chiffrment en foocntion de la valeur du keyid
+		Claims claims = Jwts.parser().setSigningKeyResolver(new SigningKeyResolverAdapter() {
+	        @Override
+	        public byte[] resolveSigningKeyBytes(JwsHeader header, Claims claims) {
+	            //inspect the header or claims, lookup and return the signing key
+	            String keyId = header.getKeyId(); //or any other field that you need to inspect
+	            
+	            if (keyId.equals("VIP")) 
+	            {
+		            return DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString(JwtGenerator.SPECIALKEY.getBytes()));
+
+	            }
+	            else 
+	            {
+	            	return DatatypeConverter.parseBase64Binary(generateKey());
+	            }
+	        }}).parseClaimsJws(jwt).getBody();
+		
+		System.out.println("ID: " + claims.getId());
+		System.out.println("Subject: " + claims.getSubject());
+		System.out.println("Issuer: " + claims.getIssuer());
+		System.out.println("Expiration: " + claims.getExpiration());
+	
+		return claims;
+	}
+	
+
 }
--- src/com/mika/jwt/JwtGeneratorTest.java
+++ src/com/mika/jwt/JwtGeneratorTest.java
@@ -8,6 +8,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+
 public class JwtGeneratorTest {
 
 	@BeforeClass
@@ -27,12 +31,75 @@ public class JwtGeneratorTest {
 	}
 
 	@Test
-	public void test() {
-		String jwt=JwtGenerator.createJWT("michael", "ceci est un test", "merci", 1000000);
+	public void testEncryptDecryptJetonNormal() {
+		String jwt=JwtGenerator.createJWT("1", "Issuer1", "acces", 1000000,false);
 		assertNotNull(jwt);
 		System.out.println(jwt);
-		JwtGenerator.parseJWT(jwt);
-		assert(true);
+		Claims jetton=JwtGenerator.parseJWTwithKey(jwt);
+		assertNotNull(jetton);
+		assertEquals(jetton.getId(),"1");
+		assertEquals(jetton.getIssuer(),"Issuer1");
 	}
+	
+	@Test
+	public void testEncryptDecryptJetonVIP() {
+		String jwt=JwtGenerator.createJWT("2", "Issuer2", "access", 1000000,true);
+		assertNotNull(jwt);
+		System.out.println(jwt);
+		Claims jetton=JwtGenerator.parseJWTwithKeyhintinKeyID(jwt);
+		assertNotNull(jetton);
+		assertEquals(jetton.getId(),"2");
+	}
+	
+	@Test
+	public void testexperation () {
+		String jwt=JwtGenerator.createJWT("2", "Issuer2", "access", 10,true);
+		assertNotNull(jwt);
+		System.out.println(jwt);
+		Claims jetton=null;
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		
+			e.printStackTrace();
+		}
+		try
+		{
+			jetton=JwtGenerator.parseJWTwithKeyhintinKeyID(jwt);
+			// le jeton est normalement exprirée
+			fail();
+		}
+		catch (ExpiredJwtException e)
+		{
+			assertNull(jetton);
+		}
+		
+	}
+	
+	
+	@Test
+	public void testdecryptageKOavecClefNormal() {
+		String jwt=JwtGenerator.createJWT("2", "Issuer2", "access", 1000000, true);
+		assertNotNull(jwt);
+		System.out.println(jwt);
+		Claims jetton=null;
+		try
+		{
+			jetton=JwtGenerator.parseJWTwithKey(jwt);
+			// si on arrive la c'est qu'il y a un pb puisque le jeton ne peut pas etre decrypté avec la clef normal
+			fail();
+			
+		}
+		catch( Exception e)
+		{
+			
+		}
+		assertNull(jetton);
+		
+	}
+	
 
+	
+	
 }
